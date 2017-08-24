172a4b512b50532fe85089c0c18b6fb75a936d29
sorskoot <mental_c@hotmail.com>
2017-08-24 21:36:47 +0200

    Doubling rooms + placement

--- src/map2.js
+++ src/map2.js
@@ -26,33 +26,49 @@ var pieces = [
         2, 0,
     ],
 ];
-
-let rnd = m => ~~(Math.random() * m);
-
 var mapWidth = 64;
 
+let rnd = m => ~~(Math.random() * m);
+let pl = pieces.length;
+ for (let i = 0; i < pl; i++) {
+     pieces.push(pieces[i].slice(0,2).concat(pieces[i].slice(2).reverse()));
+ }
+ 
+pl = pieces.length;
 var map = {
     tiles: Array(mapWidth * mapWidth).fill(0)
 };
 /// To Index
 var toi = (x, y) => y * mapWidth + x;
-
-var addRoom=(pos)=>{
-    let piece = rnd(3);
+pieces[0].top=[2];
+var addRoom = function(pos){
+    let piece = rnd(pl);
     // let pos = rnd(toi(mapWidth - pieces[piece][0], 
     //                   mapWidth - pieces[piece][1]));
-    console.log(pos);
+    
+    //
+    //  50  51  52
+    // 150 151 152 
+    // 250 251 153
+  
     pieces[piece].slice(2).map((v, i) => {
         let y = ~~(i / pieces[piece][0]);
         let x = i % pieces[piece][0];
-        let ty = (pos % mapWidth + y);
-        let tp = toi((pos%mapWidth) + x, ty);
+        let ty = ~~(pos / mapWidth) + y;
+        let tx = pos%mapWidth + x;
+        let tp = toi(tx, ty);
+        console.log(pieces[piece].top);
         if (tp < mapWidth * mapWidth) { map.tiles[tp] = v; }
     });
 }
 //for (let i = 0; i < 25; i++) {
-    addRoom(toi(mapWidth/2,mapWidth/2));
+    addRoom(toi(32,32) );
 //}
+
+for (let i = 0; i < 25; i++) {
+    addRoom(rnd(mapWidth * mapWidth ));
+}
+
 
 var Renderer = {
     canvas: null,
@@ -69,7 +85,7 @@ var Renderer = {
     Update: function () {
         for (var y = 0; y < mapWidth; y++) {
             for (var x = 0; x < mapWidth; x++) {
-                pos = toi(x, y);
+                let pos = toi(x, y);
                 var tile = map.tiles[pos];
                 if (tile == 0) this.ctx.fillStyle = '#351330';
                 else if (tile == 1) this.ctx.fillStyle = '#64908A';
