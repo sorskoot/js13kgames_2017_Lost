4ed38e7ca8e5a78d260e78e2d9e066b6ade6d270
sorskoot <mental_c@hotmail.com>
2017-08-24 22:10:32 +0200

    small improvement on the mapgen

--- src/map2.js
+++ src/map2.js
@@ -1,7 +1,7 @@
 var pieces = [
     [
         5, 5,
-        0, 0, 0, 2, 0,
+        0, 0, 2, 1, 0,
         2, 1, 1, 1, 1,
         0, 1, 1, 1, 1,
         1, 1, 0, 1, 0,
@@ -25,48 +25,62 @@ var pieces = [
         1, 0,
         2, 0,
     ],
+
+    [
+        5, 2,
+        0, 1, 1, 1, 2, 
+        2, 1, 1, 0, 0
+    ],
 ];
 var mapWidth = 64;
 
 let rnd = m => ~~(Math.random() * m);
 let pl = pieces.length;
- for (let i = 0; i < pl; i++) {
-     pieces.push(pieces[i].slice(0,2).concat(pieces[i].slice(2).reverse()));
- }
- 
+for (let i = 0; i < pl; i++) {
+    pieces.push(pieces[i].slice(0, 2).concat(pieces[i].slice(2).reverse()));
+}
 pl = pieces.length;
+pieces.forEach(e => {
+    e.top = [];
+    e.slice(2, e[0]+2).map((v, i, a) => {
+        if (v === 2) e.top.push(i);
+    })
+})
+
 var map = {
     tiles: Array(mapWidth * mapWidth).fill(0)
 };
 /// To Index
 var toi = (x, y) => y * mapWidth + x;
-pieces[0].top=[2];
-var addRoom = function(pos){
+pieces[0].top = [2];
+var RoomsToBuild = 25;
+
+var addRoom = function (pos) {
     let piece = rnd(pl);
     // let pos = rnd(toi(mapWidth - pieces[piece][0], 
     //                   mapWidth - pieces[piece][1]));
-    
+
     //
     //  50  51  52
     // 150 151 152 
-    // 250 251 153
-  
+    // 250 251 153    
     pieces[piece].slice(2).map((v, i) => {
         let y = ~~(i / pieces[piece][0]);
         let x = i % pieces[piece][0];
         let ty = ~~(pos / mapWidth) + y;
-        let tx = pos%mapWidth + x;
-        let tp = toi(tx, ty);
-        console.log(pieces[piece].top);
+        let tx = pos % mapWidth + x;
+        let tp = toi(tx, ty);        
         if (tp < mapWidth * mapWidth) { map.tiles[tp] = v; }
     });
+
+    console.log(pieces[piece].top);
 }
 //for (let i = 0; i < 25; i++) {
-    addRoom(toi(32,32) );
+addRoom(toi(32, 32));
 //}
 
 for (let i = 0; i < 25; i++) {
-    addRoom(rnd(mapWidth * mapWidth ));
+    addRoom(rnd(mapWidth * mapWidth));
 }
 
 
