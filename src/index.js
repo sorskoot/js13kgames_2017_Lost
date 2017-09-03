a9272bba27c36822a87b2da3016dac9c69d42d99
sorskoot <mental_c@hotmail.com>
2017-09-03 23:51:14 +0200

    start with raycasting

--- src/index.js
+++ src/index.js
@@ -38,27 +38,23 @@ AFRAME.registerGeometry('mapgeo', {
 });
 AFRAME.registerComponent('map', {
     init: function () {
-
-
         let size = 32;
         let depth = 4;
         this.mapContext = mapGen.go(size, depth);
-
-
         let world = document.createElement("a-entity")
         let worldData = this.mapContext.getImageData(0, 0, size, size);
-
         for (let i = 0; i < size; i++) {
             for (let j = 0; j < size; j++) {
                 if (worldData.data[(i + size * j) * 4] != 0) {
 
                     let plane = document.createElement("a-entity")
+                    plane.classList.add("floor");
                      if (Math.random() > .5) {
                          plane.setAttribute("mytexture", "src: #my-texture;index:0");
                      }
-                    //  plane.addEventListener('mouseenter', function () {
-                    //     plane.setAttribute("mytexture", "tintAmount:0.5");
-                    //   });
+                      plane.addEventListener('click', function () {
+                         plane.setAttribute("mytexture", "src: #my-texture;index:4");
+                       });
                     //   plane.addEventListener('mouseleave', function () {
                     //     plane.setAttribute("mytexture", "tintAmount:0.5");
                     //   });
