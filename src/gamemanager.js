7e1ec5577b3c5575e1aa19df9fe2a54d7b61a279
sorskoot <mental_c@hotmail.com>
2017-09-08 08:41:37 +0200

    bag placement

--- src/gamemanager.js
+++ src/gamemanager.js
@@ -25,22 +25,46 @@ AFRAME.registerComponent('gamemanager', {
         var s = this;
         if (s.data.state != 0) return;
         var coords = s.player.components.position.data;
-        var a = new THREE.Vector2(data.x, data.y);                      // 0,0
-        var b = new THREE.Vector2(coords.x, coords.z);                  // 1,0
+        var a = new THREE.Vector3(data.x, data.y, 0);                      // 0,0
+        var b = new THREE.Vector3(coords.x, coords.z, 0);                  // 1,0
         coords.r = GM.camera.rot;
         if (a.distanceTo(b) > 1.5)
             return;
+        // let pi = Math.PI;
+        // let crots =
+        //     [[135, 180, 225],
+        //     [90, undefined, 270],
+        //     [45, 360, 315]];
+        // let startRot = GM.camera.rot || 0,
+        //     targetRot = crots[b.y - a.y + 1][b.x - a.x + 1],
+        //     deltaRot = 0;
+        // //   if (targetRot > pi) targetRot = targetRot % (2 * pi);
+        // // modify targetrot based on left or right turn with respect to 2PI == 0
+        //         var direction = targetRot;//Math.atan2(b.y - a.y, b.x - a.x) * 180 / Math.PI;
+        // coords.dir = direction;
+        // if (targetRot != undefined) {
+        //     deltaRot = startRot < targetRot ? .45 : -.45;
+        // }
+        // console.log(b.y - a.y, b.x - a.x, direction);
+        // coords.dir = GM.camera.components.rotation.data.y;
+        // var cp = GM.camera.components.position.data;
+        // var cv = new THREE.Vector3(cp.x, cp.y, cp.z);
+        // GM.camera.rot += deltaRot;
+        // cv.applyAxisAngle(new THREE.Vector3(0, 1, 0), direction / 180.0 * pi);
+        // GM.camera.setAttribute('position', `${cv.x} 1.2 ${cv.z}`);
+        // GM.camera.components.rotation.data.y = direction;
         var tween = new TWEEN.Tween(coords) // Create a new tween that modifies 'coords'.
-            .to({ x: data.x, z: data.y}, 1000) // Move to (300, 200) in 1 second.
+            .to({ x: data.x, z: data.y }, 1000) // Move to (300, 200) in 1 second.
             .easing(TWEEN.Easing.Quadratic.Out) // Use an easing function to make the animation smooth.
             .onUpdate(function () { // Called after tween.js updates 'coords'.
                 // Move 'box' to the position described by 'coords' with a CSS translation.
                 //box.style.setProperty('transform', 'translate(' + coords.x + 'px, ' + coords.y + 'px)');
                 s.player.setAttribute('position', `${coords.x} .25 ${coords.z}`);
-                var cp = GM.camera.components.position.data;
-                var cv = new THREE.Vector3(cp.x, cp.y, cp.z);
-                cv.applyAxisAngle(new THREE.Vector3(0, 1, 0), .01);
-                GM.camera.setAttribute('position', `${cv.x} 1.2 ${cv.z}`);
+                // rotation is relative;
+                //  if (GM.camera.rot < targetRot) {
+              
+                //GM.camera.components.rotation.data.y = coords.dir;
+                // }
             })
             .onComplete(function () {
                 s.data.state = 1;
--- src/mapcomp.js
+++ src/mapcomp.js
@@ -2,16 +2,15 @@ const mapGen = require("./mapgen");
 
 AFRAME.registerComponent('map', {
     init: function () {
-        let size = 128;
-        let depth = 7;
+        const size = 128;
+        const depth = 7;
         this.mapContext = mapGen.go(size, depth);
-        let world = document.createElement("a-entity")
+        let world = document.createElement("a-entity");
         let worldData = this.mapContext.getImageData(0, 0, size, size);
         for (let i = 0; i < size; i++) {
             for (let j = 0; j < size; j++) {
                 let wdp = (i + size * j) * 4;
                 if (worldData.data[wdp] != 0) {
-
                     let plane = document.createElement("a-entity")
                     plane.classList.add("floor");
                     if (worldData.data[wdp] == 102) plane.setAttribute("mytexture", "index:11;lookup:11");
@@ -19,7 +18,7 @@ AFRAME.registerComponent('map', {
                     else if (worldData.data[wdp] == 238) plane.setAttribute("mytexture", "index:12;lookup:9");
                     //if (worldData.data[wdp]==102) plane.setAttribute("mytexture", "index:11;lookup:8");
                     else {
-                        plane.setAttribute("mytexture", "index:0;lookup:" + ~~((i*5)/(j*2)*3) % 5);
+                        plane.setAttribute("mytexture", "index:0;lookup:" + ~~((i * 5) / (j * 2) * 3) % 5);
                     }// + rnd(6));
                     let x = i - (size / 2);
                     let y = j - (size / 2);
@@ -36,14 +35,32 @@ AFRAME.registerComponent('map', {
                 }
             }
         }
+
+        let items = document.createElement("a-entity");
         for (let i = 0; i < 25; i++) {
-            let bag = document.createElement("a-entity");
-            bag.setAttribute("billboard-texture", "index:3;lookup:-1");
-            bag.setAttribute('position', `${window.rnd(32) - 16} .25 ${window.rnd(32) - 16}`);
-            bag.setAttribute('mixin', 'spr');
-            world.appendChild(bag);
+            let b = document.createElement("a-entity"), x, y, c, tx, ty;
+            do {
+                x = window.rnd(size);
+                y = window.rnd(size);
+                c = this.mapContext.getImageData(x, y, 1, 1);
+            } while (c.data.every(p => p == 0));
+      
+            c.data[1] = 0x44;
+            c.data[3] = 255;
+            this.mapContext.putImageData(c, x, y);
+
+            tx = x - size / 2;
+            ty = y - size / 2;
+            var d = (new THREE.Vector2(0, 0)).distanceTo(new THREE.Vector2(tx, ty));
+
+            b.setAttribute("billboard-texture", "index:3;lookup:-1");
+            b.setAttribute('position', `${tx} .25 ${ty}`);
+            b.setAttribute('mixin', 'spr');
+            b.data = { dist: d };
+            items.appendChild(b);
         }
 
         this.el.appendChild(world);
+        this.el.appendChild(items);
     }
 });
