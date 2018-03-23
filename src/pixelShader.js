273513c8d95eebd03a0e8345612f0f517930f36d
Timmy Kokke <mental_c@hotmail.com>
2018-03-23 23:55:40 +0100

    cleaned pixelShader.js #34

--- src/pixelShader.js
+++ src/pixelShader.js
@@ -2,20 +2,17 @@
 AFRAME.registerComponent('pixel-shader', {
   schema: {
     color:
-    {
-      type: 'color'
-    },
+      {
+        type: 'color'
+      },
     index: { type: 'int' },
     lookup: { type: 'int', default: 0 }
   },
-  /**
-   * Creates a new THREE.ShaderMaterial using the two shaders defined
-   * in vertex.glsl and fragment.glsl.
-   */
+
   init: function () {
     const data = this.data;
-    var fragmentShader = document.getElementById('fragment').textContent;
-    var vertexShader = document.getElementById('vertex').textContent;
+    let fragmentShader = document.getElementById('fragment').textContent,
+      vertexShader = document.getElementById('vertex').textContent;
 
     this.material = new THREE.ShaderMaterial({
       uniforms: {
@@ -28,7 +25,7 @@ AFRAME.registerComponent('pixel-shader', {
         repeat: { value: { x: 1.0, y: 1.0 } },
         fogStart: { value: 5 },
         fogEnd: { value: 15 },
-        lookupIndex:{value:data.lookup},
+        lookupIndex: { value: data.lookup },
         fogColor: { value: new THREE.Color(0, 0, 0) },
         tint: { value: new THREE.Color(255, 255, 255) },
         tintAmount: { value: 0 }
@@ -39,16 +36,12 @@ AFRAME.registerComponent('pixel-shader', {
     this.applyToMesh();
     this.el.addEventListener('model-loaded', () => this.applyToMesh());
   },
-  /**
-   * Update the ShaderMaterial when component data changes.
-   */
+
   update: function () {
     this.material.uniforms.index.value = this.data.index;
-    this.material.uniforms.lookupIndex.value=this.data.lookup;
+    this.material.uniforms.lookupIndex.value = this.data.lookup;
   },
-  /**
-   * Apply the material to the current entity.
-   */
+
   applyToMesh: function () {
     const mesh = this.el.getObject3D('mesh');
     if (mesh) {
@@ -56,11 +49,5 @@ AFRAME.registerComponent('pixel-shader', {
       this.material.uniforms.repeat.value.y = +this.el.getAttribute("height") || 1;
       mesh.material = this.material;
     }
-  },
-  /**
-   * On each frame, update the 'time' uniform in the shaders.
-   */
-  // tick: function (t) {
-  //   this.material.uniforms.time.value = t / 1000; 
-  // }
+  }
 })
