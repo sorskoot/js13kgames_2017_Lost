8b14a1f8ab867c81922272186bf4f0f0a8a6adbf
Timmy Kokke <mental_c@hotmail.com>
2018-03-24 00:02:16 +0100

    cleaned mob.js for #34

--- src/mob.js
+++ src/mob.js
@@ -10,21 +10,25 @@ AFRAME.registerComponent('mob', {
     },
     move: function () {
         // A* would be nice... for now, just move random...
-        if (this.data.health <= 0) return;
-        let tx, ty, c, s = this,
-            playerPosition = GM.player.components.position.data;
-        var a = new THREE.Vector3(playerPosition.x, playerPosition.z, 0);                      // 0,0
-        var b = new THREE.Vector3(this.pos.data.x, this.pos.data.z, 0);
-        if (a.distanceTo(b) < 1.9) {// player is next to the mob attack
-            tx = a.x - b.x;
-            ty = a.y - b.y;
-            let coords = { x: s.pos.data.x, z: s.pos.data.z };
-            var tween = new TWEEN.Tween(coords) // Create a new tween that modifies 'coords'.
-                .to({ x: s.pos.data.x + tx, z: s.pos.data.z + ty }, 500) // Move to (300, 200) in 1 second.
+        if (this.data.health <= 0) {
+            return;
+        }
+        let tx, ty, color, self = this,
+            playerPosition = new THREE.Vector3(GM.player.components.position.data.x, GM.player.components.position.data.z, 0),
+            mobPosition = new THREE.Vector3(this.pos.data.x, this.pos.data.z, 0);
+
+        if (playerPosition.distanceTo(mobPosition) < 1.9) {// player is next to the mob attack
+            tx = playerPosition.x - mobPosition.x;
+            ty = playerPosition.y - mobPosition.y;
+
+            let coords = { x: self.pos.data.x, z: self.pos.data.z };
+
+            var tween = new TWEEN.Tween(coords)
+                .to({ x: self.pos.data.x + tx, z: self.pos.data.z + ty }, 500)
                 .yoyo(true).repeat(1)
-                .easing(TWEEN.Easing.Quadratic.InOut) // Use an easing function to make the animation smooth.
-                .onUpdate(function () { // Called after tween.js updates 'coords'.
-                    s.el.setAttribute('position', `${coords.x} .25 ${coords.z}`);
+                .easing(TWEEN.Easing.Quadratic.InOut)
+                .onUpdate(function () {
+                    self.el.setAttribute('position', `${coords.x} .25 ${coords.z}`);
                 })
                 .onComplete(() => {
                     //show splat
@@ -38,28 +42,28 @@ AFRAME.registerComponent('mob', {
             do {
                 tx = rnd(3) - 1;
                 ty = rnd(3) - 1;
-                c = GM.map.getPix(s.data.x + tx, s.data.y + ty);
+                color = GM.map.getPix(self.data.x + tx, self.data.y + ty);
                 tries--;
-                if(tries === 0){
+                if (tries === 0) {
                     return;
                 }
-            } while (c.data[0] === 0 || c.data[2] !== 0)
+            } while (color.data[0] === 0 || color.data[2] !== 0)
 
-            let old = GM.map.getPix(s.data.x, s.data.y);
+            let old = GM.map.getPix(self.data.x, self.data.y);
             old.data[2] = 0;
-            GM.map.putPix(old, s.data.x, s.data.y);
-            c.data[2] = 0x88;
-            s.data.x += tx;
-            s.data.y += ty;
-            GM.map.putPix(c, s.data.x, s.data.y);
-            let coords = { x: s.pos.data.x, z: s.pos.data.z };
+            GM.map.putPix(old, self.data.x, self.data.y);
+            color.data[2] = 0x88;
+            self.data.x += tx;
+            self.data.y += ty;
+            GM.map.putPix(color, self.data.x, self.data.y);
+            let coords = { x: self.pos.data.x, z: self.pos.data.z };
             var tween = new TWEEN.Tween(coords)
-                .to({ x: s.pos.data.x + tx, z: s.pos.data.z + ty }, 400 + rnd(400))
+                .to({ x: self.pos.data.x + tx, z: self.pos.data.z + ty }, 400 + rnd(400))
                 .delay(rnd(250))
                 .easing(TWEEN.Easing.Quadratic.InOut)
                 .onUpdate(function () {
-                    s.el.setAttribute('position', `${coords.x} .25 ${coords.z}`);
-                    s.el.id = `e${coords.x + size / 2}-${coords.z + size / 2}`;
+                    self.el.setAttribute('position', `${coords.x} .25 ${coords.z}`);
+                    self.el.id = `e${coords.x + size / 2}-${coords.z + size / 2}`;
                 })
                 .start();
         }
