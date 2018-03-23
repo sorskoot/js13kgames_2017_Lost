9ee3bd4e670e0b8670582f548366aff0038891b8
Timmy Kokke <mental_c@hotmail.com>
2018-03-23 23:14:59 +0100

    cleaned billboardshader.js

--- src/billboardShader.js
+++ src/billboardShader.js
@@ -1,28 +1,29 @@
 /* global THREE, AFRAME */
 AFRAME.registerComponent('billboard-shader', {
   schema: {
-    index:{type:'int'},
+    index: { type: 'int' },
     lookup: { type: 'int', default: -1 }
   },
+  
   init: function () {
-    const data = this.data;    
-    var fragmentShader = document.getElementById('fragment').textContent;
-    var vertexShader = document.getElementById('vertex-billboard').textContent;
-    
+    const data = this.data;
+    let fragmentShader = document.getElementById('fragment').textContent;
+    let vertexShader = document.getElementById('vertex-billboard').textContent;
+
     this.material = new THREE.ShaderMaterial({
       uniforms: {
         time: { value: 0.0 },
         index: { value: this.data.index },
-        DiffuseTexture: { value: window.t},
+        DiffuseTexture: { value: window.t },
         color: { value: new THREE.Color(data.color) },
         spriteDimensions: { value: { x: 16.0, y: 1.0 } },
         repeat: { value: { x: 1.0, y: 1.0 } },
-        Lookup: { value: window.pal },    
-        lookupIndex:{value:data.lookup},
-        fogStart:{value:5},
-        fogEnd:{value:15},
-        alphatest:{value:0.95},
-        fogColor:{value: new THREE.Color(0,0,0)}
+        Lookup: { value: window.pal },
+        lookupIndex: { value: data.lookup },
+        fogStart: { value: 5 },
+        fogEnd: { value: 15 },
+        alphatest: { value: 0.95 },
+        fogColor: { value: new THREE.Color(0, 0, 0) }
       },
       vertexShader,
       fragmentShader
@@ -31,6 +32,7 @@ AFRAME.registerComponent('billboard-shader', {
     this.applyToMesh();
     this.el.addEventListener('model-loaded', () => this.applyToMesh());
   },
+
   /**
    * Update the ShaderMaterial when component data changes.
    */
@@ -38,21 +40,16 @@ AFRAME.registerComponent('billboard-shader', {
     this.material.uniforms.index.value = this.data.index;
     this.material.uniforms.lookupIndex.value = this.data.lookup;
   },
+
   /**
    * Apply the material to the current entity.
    */
   applyToMesh: function () {
     const mesh = this.el.getObject3D('mesh');
     if (mesh) {
-      this.material.uniforms.repeat.value.x =+this.el.getAttribute("width")||1;
-      this.material.uniforms.repeat.value.y =+this.el.getAttribute("height")||1;
+      this.material.uniforms.repeat.value.x = +this.el.getAttribute("width") || 1;
+      this.material.uniforms.repeat.value.y = +this.el.getAttribute("height") || 1;
       mesh.material = this.material;
     }
-  },
-  /**
-   * On each frame, update the 'time' uniform in the shaders.
-   */
-  // tick: function (t) {
-  //   this.material.uniforms.time.value = t / 1000; 
-  // }
+  }
 })
