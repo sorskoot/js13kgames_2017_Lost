c091b120b208a5cbda4ddc0e7190ecd644f59aed
Timmy Kokke <mental_c@hotmail.com>
2018-03-23 23:22:14 +0100

    cleaned up canvastext.js

--- src/canvastext.js
+++ src/canvastext.js
@@ -6,40 +6,42 @@ AFRAME.registerComponent('canvas-text', {
         this.update();
     },
     update: function () {
-        var bitmap = document.createElement('canvas');
-        var g = bitmap.getContext('2d');
+        let bitmap = document.createElement('canvas');
+        let context = bitmap.getContext('2d');
         bitmap.width = 1024;
         bitmap.height = 1024;
         bitmap.style.imageRendering = 'pixelated';
-        g.font = 'bold 40px Arial';
-        g.textBaseline = 'middle';
-        g.textAlign = 'center';
-        this.multiline(g, this.data.text, 512, 512);
-        var texture = new THREE.Texture(bitmap)
+        context.font = 'bold 40px Arial';
+        context.textBaseline = 'middle';
+        context.textAlign = 'center';
+        this.multiline(context, this.data.text, 512, 512);
+        let texture = new THREE.Texture(bitmap)
         texture.needsUpdate = true;
         this.el.setAttribute('material', { src: texture, transparent: true, opacity:0.8 });
     },
+
     multiline: function (ctx, text, x, y) {
-        let lh = ctx.measureText("M").width * 1.2,
-            t = text.split("\n");
-        for (var i = 0; i < t.length; ++i) {
+        let lineheight = ctx.measureText("M").width * 1.2,
+            linesOftext = text.split("\n");
+        for (let i = 0; i < linesOftext.length; ++i) {
             ctx.fillStyle = '#be2633';
-            ctx.fillText(t[i], x, y);
+            ctx.fillText(linesOftext[i], x, y);
             ctx.strokeStyle = '#000';
-            ctx.strokeText(t[i], x, y);
-            y += lh;
+            ctx.strokeText(linesOftext[i], x, y);
+            y += lineheight;
         }
     },
+
     write: function (text, dontFade) {
         this.data.text = text;
         this.update();
-        var d = { o: 0.8 };
+        let materialProperties = { opacity: 0.8 };
         if (!dontFade) {
-            this.t = new TWEEN.Tween(d)
-                .to({ o: 0 }, 250)
+            this.tween = new TWEEN.Tween(materialProperties)
+                .to({ opacity: 0 }, 250)
                 .delay(2000)
                 .onUpdate(() => {
-                    this.el.setAttribute('material', { opacity: d.o });
+                    this.el.setAttribute('material', { opacity: materialProperties.opacity });
                 })
                 .start();
         }
