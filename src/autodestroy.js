62c5b6628136fdd58eb42ebf0b25d798c2b853ba
Timmy Kokke <mental_c@hotmail.com>
2018-03-23 23:13:16 +0100

    refacored Autodestroy

--- src/autodestroy.js
+++ src/autodestroy.js
@@ -1,21 +1,24 @@
 AFRAME.registerComponent('auto-destroy', {
     init: function () {
-        // tween and destroy
-        var m = this.el.getObject3D('mesh');
-        if (m) m.material.depthTest = false;
-        let oldx = this.el.components.position.data.x || 0,
-            oldy = this.el.components.position.data.y || 0,
-            oldz = this.el.components.position.data.z || 0,
-            coords = { y: oldy }
-        var tween = new TWEEN.Tween(coords) // Create a new tween that modifies 'coords'.
-            .to({ y: oldy - .25 }, 500) // Move to (300, 200) in 1 second.
-            .easing(TWEEN.Easing.Quadratic.In) // Use an easing function to make the animation smooth.
-            .onUpdate(() => { // Called after tween.js updates 'coords'.
-                this.el.setAttribute('position', { x: oldx, y: coords.y, z: oldz });
+        let mesh = this.el.getObject3D('mesh');
+        if (mesh){
+            mesh.material.depthTest = false;
+        }
+        let oldCoords = {
+                x: this.el.components.position.data.x || 0,
+                y: this.el.components.position.data.y || 0,
+                z: this.el.components.position.data.z || 0},
+            newCoords = { y: oldCoords.y };
+
+        let tween = new TWEEN.Tween(newCoords)
+            .to({ y: oldCoords.y  - .25 }, 500) 
+            .easing(TWEEN.Easing.Quadratic.In) 
+            .onUpdate(() => { 
+                this.el.setAttribute('position', { x: oldCoords.x, y: newCoords.y, z: oldCoords.z  });
             })
             .onComplete(() => {
                 this.el.parentNode.removeChild(this.el);
             })
-            .start(); // Start the tween immediately.
+            .start(); 
     }
 });
